932d78ec8560abf657faf90d57f32fe5c93d05b8
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2025-12-20 00:48:33 +0300

    created code for js scope

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+// Lab 2 Solution: JavaScript Scope
+
+// Global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function-scoped variable inside addBurger
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+// Block-scoped variable inside if statement
+if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+// Function to change global variable featuredDrink
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
+
+// Example usage:
+addBurger();
+console.log(burgers); // ['Hamburger', 'Cheeseburger', 'Flatburger', 'Maple Bacon Burger']
+changeFeaturedDrink();
+console.log(featuredDrink); // 'The JavaShake'
 // Write your solution in this file!
